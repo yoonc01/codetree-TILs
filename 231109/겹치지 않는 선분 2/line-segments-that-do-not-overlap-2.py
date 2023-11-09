c092448d77b0a51e9dae51eb4babff9e757c3a49
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2023-11-09 22:17:09 +0900

    Title: 겹치지 않는 선분 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231109/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+points = []
+
+for _ in range(n):
+    points.append(list(map(int, input().split())))
+
+cnt = 0
+for i in range(n - 1):
+    x1, y1 = points[i]
+    overlap = 0
+    for j in range(i + 1, n):
+        x2, y2 = points[j]
+        if (x2 - x1) * (y2 - y1) < 0:
+            overlap = 1
+            break
+    if (overlap == 0):
+        cnt = cnt + 1
+print(cnt)
