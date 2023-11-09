04d6690f332d5debe6b71ea9b65be12f19b77ac3
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2023-11-09 23:53:39 +0900

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231109/정수 복사/copy-integer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+
+    a = 3;
+    b = 4;
+    b = a;
+    cout << a << " " << b << endl;
+    cout << a * b;
+    return 0;
+}
