a36796e11818bc51abf2ff72bb477685a0195aff
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2023-11-09 23:39:25 +0900

    Title: 달에서 무게 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231109/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int w = 13;
+    double g = 0.165;
+
+    cout << fixed;
+    cout.precision(6);
+
+    coutu << w << " * " << g << " = " << w * g;
+    return 0;
+}
