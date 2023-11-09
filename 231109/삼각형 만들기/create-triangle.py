39f5531174fcccd1c579419a572f4fb69d3d2568
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2023-11-09 21:58:08 +0900

    Title: 삼각형 만들기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231109/삼각형 만들기/create-triangle.py
+++ 231109/삼각형 만들기/create-triangle.py
@@ -26,7 +26,7 @@ for i in range(n - 2):
             x2, y2 = dots[j]
             x3, y3 = dots[k]
 
-            s = (x1*y2 + x2*y3 + x3*y1 - x2*y1 - x3*y2 - x1*y3)
+            s = abs(x1*y2 + x2*y3 + x3*y1 - x2*y1 - x3*y2 - x1*y3)
             if ans < s:
                 ans = s
 print(s)
