eaee6f58691eda16832bbd971e0d1dae71cdae5b
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2023-11-11 17:27:34 +0900

    Title: 독서실의 거리두기 5, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231111/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -0,0 +1,19 @@
+import sys
+
+n = int(input())
+
+sen = input()
+
+seats = []
+for i in range(len(sen)):
+    if sen[i] == "1":
+        seats.append(i)
+
+ans = 0
+for i in range(n):
+    dis = sys.maxsize
+    for seat in seats:
+        dis = min(dis, abs(seat - i))
+    ans = max(ans, dis)
+
+print(ans)
