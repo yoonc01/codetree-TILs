c640c4a0f6ba5bef82d99fd0b17c2c4579754861
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2023-11-11 17:34:21 +0900

    Title: 독서실의 거리두기 5, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 231111/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 231111/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,22 +1,30 @@
 n = int(input())
+seat = list(input())
 
-sen = input()
 
-seats = []
-for i in range(len(sen)):
-    if sen[i] == "1":
-        seats.append(i)
+def min_dist():
+    dist = n
+    # 둘 다 1인 곳에 대해
+    # 모든 쌍을 조사하여, 그 중 가장 가까운 거리를 구합니다.
+    for i in range(n):
+        for j in range(i + 1, n):
+            if seat[i] == '1' and seat[j] == '1':
+                dist = min(dist, j - i)
+    
+    return dist
+
 
 ans = 0
+# 들어갈 위치를 일일이 정해보며
+# 그 상황에서 가장 가까운 사람간의 거리를 구해
+# 가능한 경우 중 최댓값을 계산합니다.
 for i in range(n):
-    dis = n
-    check = False
-    for seat in seats:
-        if i == seat:
-            continue
-        dis = min(dis, abs(seat - i))
-        check = True
-    if check:
-        ans = max(ans, dis)
+    if seat[i] == '0':
+        # 비어있는 위치에 인원을 배치합니다.
+        seat[i] = '1'
+        # 가장 가까운 사람간의 거리를 구해 최댓값을 갱신해줍니다.
+        ans = max(ans, min_dist())
+        # 다시 채워졌던 값을 되돌려줍니다.
+        seat[i] = '0'
 
 print(ans)
