603e072f7a14e1b4eb226a277092ba9d0f1c2fcd
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2023-11-11 14:44:22 +0900

    Title: 팀으로 하는 틱택토 2, Time: 160ms, Memory: 24MB, Status: Passed - CodeTree

--- 231111/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 231111/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -4,19 +4,25 @@ input = sys.stdin.readline
 
 G = [input().rstrip() for _ in range(3)]
 
+check = []
 ans = 0
 for line in G:
     l = list(set(line))
+    l.sort()
     if len(l) == 2:
-        ans = ans + 1
-
+        if l not in check:
+            ans = ans + 1
+        check.append(l)    
 for i in range(3):
     l = []
     for j in range(3):
         l.append(G[j][i])
         l = list(set(l))
+        l.sort()
     if len(l) == 2:
-        ans = ans + 1
+        if l not in check:
+            ans = ans + 1
+        check.append(l)    
         
 l = []
 l.append(G[0][0])
@@ -24,9 +30,10 @@ l.append(G[1][1])
 l.append(G[2][2])
 l = list(set(l))
 
-
 if len(l) == 2:
-    ans = ans + 1
+    if l not in check:
+        ans = ans + 1
+    check.append(l)   
 
 
 l = []
@@ -36,5 +43,8 @@ l.append(G[2][0])
 l = list(set(l))
 
 if len(l) == 2:
-    ans = ans + 1
+    if l not in check:
+        ans = ans + 1
+    check.append(l)   
+
 print(ans)
