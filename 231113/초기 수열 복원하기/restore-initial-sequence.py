1caa8aba4665bb1934e763d07da3a1cc97194862
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2023-11-13 22:11:30 +0900

    Title: 초기 수열 복원하기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231113/초기 수열 복원하기/restore-initial-sequence.py
@@ -0,0 +1,20 @@
+n = int(input())
+l = list(map(int, input().split()))
+
+ans = []
+for i in range(1, l[0]):
+    find = True
+    ans.append(i)
+    for k in l:
+        add = k - ans[-1]
+        if add in ans:
+            find = False
+            break
+        ans.append(add)
+    if find:
+        print(" ".join(map(str, ans)))
+        break
+    else:
+        while(ans):
+            ans.pop()
+        continue
