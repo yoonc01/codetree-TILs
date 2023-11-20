5b8b5f1dae812b36b3ef99d71cda5e8e89771339
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2023-11-20 23:51:10 +0900

    Title: 최대 최소간의 차, Time: 122ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231120/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+l = list(map(int, input().split()))
+
+ans = sys.maxsize
+
+for min_val in range(min(l) - 3, max(l) + 3):
+    cost = 0
+    for i in l:
+        if min_val <= i <= min_val + 2:
+            continue
+        elif i < min_val:
+            cost = cost + min_val - i
+        else:
+            cost = cost + i - min_val - 2
+    ans = min(cost, ans)
+print(ans)
