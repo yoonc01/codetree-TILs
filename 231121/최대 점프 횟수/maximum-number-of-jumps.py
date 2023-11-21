c6d3a8f0b3ba7be025854739c9bc79bebc854d9d
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2023-11-21 14:33:45 +0900

    Title: 최대 점프 횟수, Time: 165ms, Memory: 25MB, Status: Passed - CodeTree

--- 231121/최대 점프 횟수/maximum-number-of-jumps.py
+++ 231121/최대 점프 횟수/maximum-number-of-jumps.py
@@ -5,7 +5,7 @@ max_val = 0
 dp[0] = 0
 for i in range(1, n):
     for j in range(i):
-        if l[j] + j >= i and dp[j] != -1:
+        if dp[j] != - 1 and l[j] + j >= i:
             dp[i] = max(dp[i], dp[j] + 1)
     max_val = max(max_val, dp[i])
 print(max_val)
