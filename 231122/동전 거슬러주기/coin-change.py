d5ecc6c60385b3228c009dbfcb6a49f92cdbca8e
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2023-11-22 14:32:54 +0900

    Title: 동전 거슬러주기, Time: 135ms, Memory: 25MB, Status: Passed - CodeTree

--- 231122/동전 거슬러주기/coin-change.py
+++ 231122/동전 거슬러주기/coin-change.py
@@ -8,7 +8,7 @@ dp[0] = 0
 
 for i in range(m + 1):
     for coin in coins:
-        if i - coin >= 0:
+        if i - coin >= 0: #dp[0] = 0 을 넣었기에 dp[i - coin] != sys.maxsize라는 초기화 유무 검사 필요가 없음
             dp[i] = min(dp[i - coin] + 1, dp[i])
 
 print(dp[m] if dp[m] != sys.maxsize else -1)
