5442adb0db2b8a29a82a9b1620f50c9b2e921a8b
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-04-11 13:37:10 +0900

    Title: 가장 짧은 부분합, Time: 184ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240411/가장 짧은 부분합/shortest-subtotal.py
@@ -0,0 +1,17 @@
+n, s = map(int, input().split())
+
+l = list(map(int, input().split()))
+
+sum_val = l[0]
+j = 0
+ans = n + 1
+for i in range(n):
+    while j + 1 < n and sum_val < s:
+        sum_val = sum_val + l[j + 1]
+        j = j + 1
+
+    if sum_val >= s:
+        ans = min(ans, j - i + 1)
+    sum_val = sum_val - l[i]
+
+print(ans if ans < n + 1 else -1)
