e7f2111339e728d381b9cf90a237c0ea05c1ad72
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-04-15 10:30:58 +0900

    Title: 가장 많이 겹치는 구간 2, Time: 295ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240415/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+l = []
+
+for _ in range(n):
+    s, e = map(int, input().split())
+    l.append([s, 1])
+    l.append([e, -1])
+
+l.sort()
+
+over_lap = 0
+max_val = 0
+
+for p, v in l:
+    over_lap = over_lap + v
+    if (over_lap > max_val):
+        cnt = 1
+        max_val = over_lap
+    elif over_lap == max_val:
+        cnt = cnt + 1
+print(cnt)
