5b3638d3d4121da26c382c52d1faab36cc8f4cb6
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-04-17 23:11:55 +0900

    Title: 정수 분배하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240417/정수 분배하기/distributing-integers.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+def cal(l, mid):
+    cnt = 0
+    for i in l:
+        cnt = cnt + i // mid
+
+    return cnt
+
+n, m = map(int, input().split())
+
+l = []
+
+for _ in range(n):
+    l.append(int(input().rstrip()))
+
+left = 1
+right = min(l)
+ans = 1
+
+while(left <= right):
+    mid = (left + right) // 2
+    if cal(l, mid) >= m:
+        left = mid + 1
+        ans = max(ans, mid)
+    else:
+        right = mid - 1
+
+print(ans)
