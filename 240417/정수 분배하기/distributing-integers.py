0ce599ac6737182c6712c9440bd70c9a6190135e
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-04-17 23:17:49 +0900

    Title: 정수 분배하기, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240417/정수 분배하기/distributing-integers.py
+++ 240417/정수 분배하기/distributing-integers.py
@@ -16,16 +16,18 @@ l = []
 for _ in range(n):
     l.append(int(input().rstrip()))
 
-left = 1
+left = 0
 right = min(l)
 ans = 0
 
 while(left <= right):
     mid = (left + right) // 2
+    if mid == 0:
+        ans = 0
+        break
     if cal(l, mid) >= m:
         left = mid + 1
-        if cal(l, mid) == m:
-            ans = max(ans, mid)
+        ans = max(ans, mid)
     elif cal(l, mid) < m:
         right = mid - 1
 
