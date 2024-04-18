e6a81bdc01da633215128fdd3ec9f15cf1307ac1
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-04-18 16:12:46 +0900

    Title: 삼 오 무, Time: 165ms, Memory: 24MB, Status: Passed - CodeTree

--- 240418/삼 오 무/three-five-moo.py
+++ 240418/삼 오 무/three-five-moo.py
@@ -1,10 +1,20 @@
 n = int(input())
 
-ans = 6 * (n // 3)
+ans = 15 * (n // 8)
 
-if n % 3 == 0:
-    print(ans - 2)
-elif n % 3 == 1:
+if n % 8 == 0:
+    print(ans - 1)
+elif n % 8 == 1:
     print(ans + 1)
-else:
+elif n % 8 == 2:
     print(ans + 2)
+elif n % 8 == 3:
+    print(ans + 4)
+elif n % 8 == 4:
+    print(ans + 7)
+elif n % 8 == 5:
+    print(ans + 8)
+elif n % 8 == 6:
+    print(ans + 11)
+elif n % 8 == 7:
+    print(ans + 13)
