01bc893e6486c9b11bdba981f34aac46a3e67c17
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-04-19 12:51:21 +0900

    Title: 달리자, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240419/달리자/run-and-run.py
+++ 240419/달리자/run-and-run.py
@@ -8,15 +8,10 @@ for i in range(n):
     move.append(a[i] - b[i])
 
 ans = 0
-for i in range(n):
-    for j in range(i + 1, n):
-        if move[j] < 0:
-            if move[i] + move[j] > 0:
-                move[i] = move[i] + move[j]
-                ans = ans + -move[j] * (j - i)
-            if move[i] + move[j] <= 0:
-                ans = ans + move[i] * (j - i)
-                move[i] = 0
-                break
-                
+
+for i in range(n - 1):
+    if move[i] > 0:
+        ans = ans + move[i]
+        move[i + 1] = move[i + 1] + move[i]
+
 print(ans)
