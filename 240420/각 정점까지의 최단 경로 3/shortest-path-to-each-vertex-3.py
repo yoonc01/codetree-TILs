4775e13653f91460a142aa1d9ba2221f453adca5
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-04-20 17:35:19 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240420/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+G = [[0 for _ in range(n)] for _ in range(n)]
+
+for _ in range(m):
+    s, e, v = map(int, input().split())
+    G[s - 1][e - 1] = v
+
+visited = [False] * n
+dist = [sys.maxsize] * n
+dist[0] = 0
+
+for i in range(n):
+    min_val = -1
+
+    for j in range(n):
+        if visited[j]:
+            continue
+
+        if min_val == -1 or dist[min_val] > dist[j]:
+            min_val = j
+    visited[min_val] = True
+
+    for j in range(n):
+        if G[min_val][j] == 0:
+            continue
+        
+        dist[j] = min(dist[j], dist[min_val] + G[min_val][j])
+    
+for i in range(1, n):
+    print(dist[i])
