35bcbfb353c40348963faf4e18453aaaeea1cd41
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-04-26 19:55:26 +0900

    Title: 행렬로 주어진 간선, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240426/행렬로 주어진 간선/edge-given-by-matrix.py
+++ 240426/행렬로 주어진 간선/edge-given-by-matrix.py
@@ -9,13 +9,11 @@ G = [list(map(int, input().split())) for _ in range(n)]
 for i in range(n):
     G[i][i] = 1
 
-possible = [[0 for _ in range(n)] for _ in range(n)]
-
 for k in range(n):
     for i in range(n):
         for j in range(n):
             if G[i][k] and G[k][j]:
-                possible[i][j] = 1
+                G[i][j] = 1
 
 for i in range(n):
-    print(" ".join(map(str, possible[i])))
+    print(" ".join(map(str, G[i])))
