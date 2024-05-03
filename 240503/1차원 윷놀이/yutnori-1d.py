108a4fc00e66b7a359034ac8dbe34e84abe61335
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-03 12:26:50 +0900

    Title: 1차원 윷놀이, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240503/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,25 @@
+n, m, k = map(int, input().split())
+
+dist = list(map(int, input().split()))
+
+horse = [0] * n
+ans = 0
+def backtrack(num):
+    global ans
+    if num == n:
+        total = 0
+        for i in horse:
+            if i >= m:
+                total = total + 1
+        ans = max(ans, total)
+        return
+
+    for i in range(k):
+        if horse[i] >= m:
+            continue
+        horse[i] = horse[i] + dist[num]
+        backtrack(num + 1)
+        horse[i] = horse[i] - dist[num]
+
+backtrack(0)
+print(ans)
