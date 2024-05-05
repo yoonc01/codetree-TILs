681ffea209bf8b738b506bbb8b73a3a10f67757e
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-05 15:36:14 +0900

    Title: 1, 2, 5 더하기, Time: 99ms, Memory: 24MB, Status: Passed - CodeTree

--- 240505/1, 2, 5 더하기/1-2-5-plus.py
+++ 240505/1, 2, 5 더하기/1-2-5-plus.py
@@ -6,10 +6,10 @@ dp[0] = 1
 
 for i in range(n + 1):
     if i - 1 >= 0:
-        dp[i] = dp[i] + dp[i - 1]
+        dp[i] = (dp[i] + dp[i - 1]) %  10007
     if i - 2 >= 0:
-        dp[i] = dp[i] + dp[i - 2]
+        dp[i] = (dp[i] + dp[i - 2]) % 10007
     if i - 5 >= 0:
-        dp[i] = dp[i] + dp[i - 5]
+        dp[i] = (dp[i] + dp[i - 5]) % 10007
 
 print(dp[n])
