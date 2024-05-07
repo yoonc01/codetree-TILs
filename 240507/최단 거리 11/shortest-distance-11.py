a7a6f879744ff888c7db286650f8e0367323641c
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-07 18:21:05 +0900

    Title: 최단 거리 11, Time: 191ms, Memory: 30MB, Status: Passed - CodeTree

--- 240507/최단 거리 11/shortest-distance-11.py
+++ 240507/최단 거리 11/shortest-distance-11.py
@@ -1,44 +1,49 @@
-import heapq
 import sys
 
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-G = [[] for _ in range(n)]
+G = [[0 for _ in range(n)] for _ in range(n)]
 
 for _ in range(m):
     s, e, cost = map(int, input().split())
-    G[s - 1].append((e - 1, cost))
-    G[e - 1].append((s - 1, cost))
+    G[s - 1][e - 1] = cost
+    G[e - 1][s - 1] = cost
 
 a, b = map(int, input().split())
 
-heap = []
 dist = [sys.maxsize for _ in range(n)]
+visited = [False for _ in range(n)]
+
 dist[b - 1] = 0
 
-heapq.heappush(heap, (0, b - 1))
+for _ in range(n):
+    min_val = -1
+    for i in range(n):
+        if visited[i]:
+            continue
 
-while(heap):
-    min_dist, min_idx = heapq.heappop(heap)
-    if dist[min_idx] != min_dist:
-        continue
-    
-    for to_idx, to_dist in G[min_idx]:
-        new_dist = dist[min_idx] + to_dist
-        if dist[to_idx] > new_dist:
-            dist[to_idx] = new_dist
-            heapq.heappush(heap, (new_dist, to_idx))
+        if min_val == -1 or dist[min_val] > dist[i]:
+            min_val = i
+        
+    visited[min_val] = True
+
+    for i in range(n):
+        if G[min_val][i] != 0:
+            if dist[i] > dist[min_val] + G[min_val][i]:
+                dist[i] = dist[min_val] + G[min_val][i]
 
 print(dist[a - 1])
 
 x = a - 1
+
 print(x + 1, end = " ")
 while(x != b - 1):
-    G[x].sort()
-    for to_idx, to_dist in G[x]:
-        if dist[x] == dist[to_idx] + to_dist:
-            x = to_idx
+    for i in range(n):
+        if G[i][x] == 0:
+            continue
+        if dist[x] == dist[i] + G[i][x]:
+            x = i
             break
     print(x + 1, end = " ")
