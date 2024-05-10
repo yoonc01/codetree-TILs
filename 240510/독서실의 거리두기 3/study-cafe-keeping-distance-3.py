8d1b96be2b44cbe50e0ea48a1732c93551e676e8
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-10 13:32:13 +0900

    Title: 독서실의 거리두기 3, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240510/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 240510/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -1,13 +1,26 @@
 n = int(input())
-sen = input()
+sen = list(map(int, input()))
 
 before = 0
 dist = 0
 
 for i in range(n):
-    if sen[i] == "1":
+    if sen[i] == 1:
         if i - before > dist:
             dist = i - before
+            between = (before, i)
         before = i
 
-print(dist // 2)
+s, e = between
+ans = 0
+for i in range(s + 1, e):
+    sen[i] = 1
+    before = 0
+    dist = n
+    for j in range(1, n):
+        if sen[j] == 1:
+            dist = min(dist, j - before)
+            before = j
+    ans = max(ans, dist)
+    sen[i] = 0
+print(ans)
