d0fe59563e9e5f9739728851f9ff37abf1f8c8a7
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-11 23:31:18 +0900

    Title: 순위 경쟁2, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240511/순위 경쟁2/ranking-competition2.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+d = {"A": 0, "B": 0}
+
+n = int(input())
+
+top = ""
+ans = 0
+for _ in range(n):
+    c, s = input().split()
+    s = int(s)
+    
+    d[c] = d[c] + s
+
+    if top != "A" and d["A"] > d["B"]:
+        ans = ans + 1
+        top = "A"
+    elif top != "A and B" and d["A"] == d["B"]:
+        ans = ans + 1
+        top = "A and B"
+    elif top != "B" and d["B"] > d["A"]:
+        ans = ans + 1
+        top = "B"
+
+print(ans)
