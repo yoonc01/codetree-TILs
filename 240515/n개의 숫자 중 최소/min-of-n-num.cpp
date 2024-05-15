a736f94b29cd85ea3e58a11f5ac0996cef5500b8
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-15 20:14:34 +0900

    Title: n개의 숫자 중 최소, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240515/n개의 숫자 중 최소/min-of-n-num.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int ans = 2147483647;
+    int cnt = 1;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+
+        cin >> num;
+        if (num < ans)
+        {
+            ans = num;
+            cnt = 1;
+        }
+        else if (num == ans)
+            cnt++;
+    }
+    cout << ans << " " << cnt;
+    return 0;
+}
