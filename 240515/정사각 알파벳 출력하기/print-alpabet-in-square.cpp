6f678c20cbe42c85e5d3cbcefbf0e3d84fc2a0ba
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-15 14:48:24 +0900

    Title: 정사각 알파벳 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240515/정사각 알파벳 출력하기/print-alpabet-in-square.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    char c = 65;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << c;
+            c++;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
