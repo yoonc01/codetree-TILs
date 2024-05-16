0302a27330c757d53ad3667c41e47788e4c44b49
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-16 22:41:14 +0900

    Title: 숫자 합치기, Time: 177ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 240516/숫자 합치기/merge-numbers.py
+++ 240516/숫자 합치기/merge-numbers.py
@@ -1,13 +1,17 @@
+from collections import deque
+
 n = int(input())
 
 l = list(map(int, input().split()))
 
-ans = 0
-while(len(l) != 1):
-    l.sort(reverse = True)
-    a = l.pop()
-    b = l.pop()
-    ans = ans + a + b
-    l.append(a + b)
+l.sort()
 
+q = deque(l)
+ans = 0
+while(n > 1):
+    a = q.popleft()
+    b = q.popleft()
+    ans = ans + a + b
+    q.append(a + b)
+    n = n - 1
 print(ans)
