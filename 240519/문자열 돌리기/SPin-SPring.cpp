d94c79356f9c2a2241dd0153a5fe973a44fffba3
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-19 23:54:30 +0900

    Title: 문자열 돌리기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240519/문자열 돌리기/SPin-SPring.cpp
+++ 240519/문자열 돌리기/SPin-SPring.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> str;
 
     int len = str.length();
-    cout << str;
+    cout << str << '\n';
     for (int i = 0; i < len - 1; i++)
     {
         str = str.substr(len - 1, 1) + str.substr(0, len - 1);
