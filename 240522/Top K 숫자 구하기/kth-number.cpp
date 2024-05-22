3308694cb7b4919784c98db2dbc771736e83c5a6
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-22 18:39:28 +0900

    Title: Top K 숫자 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240522/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    int arr[1000];
+
+    cin >> n >> k;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    sort(arr, arr + n);
+    cout << arr[k - 1];
+    return 0;
+}
