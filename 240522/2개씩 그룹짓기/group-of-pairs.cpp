d80bc8c8bd9ded7f15eaff2d00bbabe3376f1f4a
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-22 18:55:43 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240522/2개씩 그룹짓기/group-of-pairs.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int arr[1000];
+
+int get_max_val(int n)
+{
+    int max_val = 2000;
+    for (int i = 0; i < n; i++)
+        max_val = min(max_val, arr[i] + arr[2 * n - 1 - i]);
+    
+    return (max_val);
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < 2 * n; i++)
+        cin >> arr[i];
+    sort(arr, arr + 2 * n);
+    cout << get_max_val(n);
+    return 0;
+}
