5687a9d23935dfa814021773fad84dbb7f276f9d
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-22 19:00:41 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 20ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240522/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string str1, str2;
+
+    cin >> str1 >> str2;
+
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+    if (str1 == str2)
+        cout << "Yes";
+    else
+        cout << "No";
+    return 0;
+}
