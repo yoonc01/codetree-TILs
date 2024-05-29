452553f9011bede32766b636570024b9019a2c01
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-29 14:23:42 +0900

    Title: 트리 노드 제거, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240529/트리 노드 제거/remove-tree-node.py
+++ 240529/트리 노드 제거/remove-tree-node.py
@@ -1,3 +1,4 @@
+from collections import deque
 import sys
 
 input = sys.stdin.readline
@@ -9,17 +10,41 @@ parents = list(map(int, input().split()))
 node = int(input())
 
 d = set()
-parents[node] = -1
 d.add(node)
 
-for i in range(n):
-    if parents[i] in d:
-        d.add(i)
-        parents[i] = -1
+G = [[] for _ in range(n)]
 
-cnt = 0
-for i in parents:
-    if i != -1:
-        cnt = cnt + 1
+for child, parent in enumerate(parents):
+    if parent == -1:
+        start = child
+        continue
+    G[parent].append(child)
 
-print(cnt)
+visited = [False for _ in range(n)]
+
+parents = [-1 for _ in range(n)]
+
+def bfs(s):
+    cnt = 0
+    visited[s] = True
+    q = deque()
+    q.append(s)
+    while(q):
+        x = q.popleft()
+        if x in d:
+            for child in G[x]:
+                if not visited[child]:
+                    visited[child] = True
+                    d.add(child)
+                    q.append(child)
+        else:
+            for child in G[x]:
+                if not visited[child]:
+                    visited[child] = True
+                    q.append(child)
+                    parents[child] = x
+            if len(G[x]) == 0:
+                cnt = cnt + 1
+    return cnt
+
+print(bfs(start))
