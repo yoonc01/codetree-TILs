7843b3f4b4c1a1baa0be04d2e8416d0df22db441
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-05-30 10:39:24 +0900

    Title: 트리 정점 거리, Time: 1492ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240530/트리 정점 거리/node-distance.py
@@ -0,0 +1,29 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+G = [[] for _ in range(n)]
+
+def bfs(s, e):
+    visited = [False for _ in range(n)]
+    visited[s] = True
+    q = deque()
+    q.append((s, 0))
+    while(q):
+        x, w = q.popleft()
+        for to_idx, to_dist in G[x]:
+            if to_idx == e:
+                return w + to_dist
+            q.append((to_idx, w + to_dist))
+
+for _ in range(n - 1):
+    s, e, w = map(int, input().split())
+    G[s - 1].append((e - 1, w))
+    G[e - 1].append((s - 1, w))
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    print(bfs(s - 1, e - 1))
