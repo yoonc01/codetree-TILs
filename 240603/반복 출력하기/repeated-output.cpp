a781b573442518a7d5cffa94c40244cae6dcecd9
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-06-03 12:18:49 +0900

    Title: 반복 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240603/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void    print(int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << "12345^&*()_" << '\n';
+}
+
+int main() {
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
