fd05bf2f176c62500d3e4804211bb8495882c647
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2024-06-19 23:23:10 +0900

    Title: 원소 10개의 합, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

--- 240619/원소 10개의 합/sum-of-10-elements.js
+++ 240619/원소 10개의 합/sum-of-10-elements.js
@@ -2,7 +2,8 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(/\s+/).map(Number);
 let sum = 0;
 
-for (let elem of input)
-    sum = sum + elem;
+input.forEach((val) => {
+    sum = sum + val;
+})
 
 console.log(sum);
