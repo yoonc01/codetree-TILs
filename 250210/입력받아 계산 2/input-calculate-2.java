ec368ddb4accabccde062dbe36841ff59bea06f9
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-10 17:35:10 +0900

    Title: 입력받아 계산 2, Time: 106ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250210/입력받아 계산 2/input-calculate-2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a, b;
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        bw.append(String.format("%d", a + b));
+        bw.flush();
+        bw.close();
+    }
+}
