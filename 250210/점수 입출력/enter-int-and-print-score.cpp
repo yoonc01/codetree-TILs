a377f691e01a23997c8afb5a513df7ec94654cec
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-10 16:11:43 +0900

    Title: 점수 입출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250210/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    cout << "Your score is " << n << " point." << endl;
+    return 0;
+}
