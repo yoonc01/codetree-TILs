ec10066e062cce737c0320172cc1f31db907e4d7
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-10 15:11:32 +0900

    Title: 달에서 무게 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250210/달에서 무게 구하기/weight-on-the-moon.java
+++ 250210/달에서 무게 구하기/weight-on-the-moon.java
@@ -1,13 +1,1 @@
-import java.util.*;
-import java.io.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        bw.write(String.format("%d * %.6f = %.6f\n", 13, 0.165000, 13 * 0.165000));
-        bw.flush();
-        bw.close();
-    }
-}
+console.log(`13 * 0.165000 = ${(13 * 0.165000).toFixed(6)}`)
