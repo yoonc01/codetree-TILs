f74aa512ca209b22e4912db3b86de5a30ee00e10
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-10 15:09:26 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250210/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+    cout.precision(6);
+    cout << 13 << " * " << 0.165000 <<" = " << 13 * 0.165000 << endl;
+    return 0;
+}
