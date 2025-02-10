245311239d7a367c7749b3827d8d3e750b0a1fdd
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-10 16:47:55 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250210/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    double num;
+    
+    cout << fixed;
+    cout.precision(2);
+
+    cin >> num;
+    cout << num << endl;
+    return 0;
+}
