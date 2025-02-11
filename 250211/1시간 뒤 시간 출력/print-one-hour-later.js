18dbe2284cd34441122482167380cd3a6cf06757
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-11 12:46:54 +0900

    Title: 1시간 뒤 시간 출력, Time: 199ms, Memory: 15MB, Status: Passed - CodeTree

--- 250211/1시간 뒤 시간 출력/print-one-hour-later.js
+++ 250211/1시간 뒤 시간 출력/print-one-hour-later.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 //정규 표현식 사용
 const input = fs.readFileSync(0).toString().trim().split(/[\s:]+/);
-const [h, m] = input;
+let [h, m] = input;
 h++;
-console.log(`{h}:{m}`);
+console.log(`${h}:${m}`);
