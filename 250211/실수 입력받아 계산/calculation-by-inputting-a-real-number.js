6312e04ebdedc0df0ccc2dad662a2b2907b57a10
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-11 09:27:57 +0900

    Title: 실수 입력받아 계산, Time: 187ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250211/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const numbers = input.map(Number);
+const [a, b] = numbers;
+
+console.log((a + b).toFixed(2));
