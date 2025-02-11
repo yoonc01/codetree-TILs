2adef0b7885ade9b5686a4a2d34f16a34466e0fd
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-11 09:13:17 +0900

    Title: 입력받아 출력, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250211/입력받아 출력/input-and-output.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int a, b;
+        st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        b = Integer.parseInt(st.nextToken());
+        bw.append(String.format("%d %d", a, b));
+        bw.flush();
+        bw.close();
+    }
+}
