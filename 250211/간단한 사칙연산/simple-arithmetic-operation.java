6e6639744fde38074be2e922f363dd560efb55e7
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-11 13:37:24 +0900

    Title: 간단한 사칙연산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250211/간단한 사칙연산/simple-arithmetic-operation.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int a, b;
+        st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        bw.append(String.format("%d\n", a + b))
+        .append(String.format("%d\n", a - b))
+        .append(String.format("%d\n", a / b))
+        .append(String.format("%d\n", a % b));
+        bw.flush();
+        bw.close();
+    }
+}
