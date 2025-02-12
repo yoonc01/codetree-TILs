27c627e3d615af8eca820371ec9645f6a73dedaa
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-12 15:01:38 +0900

    Title: 문자열에서 문자 출력, Time: 184ms, Memory: 15MB, Status: Passed - CodeTree

--- 250212/문자열에서 문자 출력/print-chars-from-word.js
+++ 250212/문자열에서 문자 출력/print-chars-from-word.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 
 const input = fs.readFileSync(0).toString().trim();
 
-for (let i = 0; i < input.length(); i++) {
+for (let i = 0; i < input.length; i++) {
     console.log(input[i]);
 }
