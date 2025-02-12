21af34c2bab9dac9035a576fbfe5df99fc5d7f00
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-12 15:50:10 +0900

    Title: Run Length 인코딩, Time: 174ms, Memory: 15MB, Status: Passed - CodeTree

--- 250212/Run Length 인코딩/run-length-encoding.js
+++ 250212/Run Length 인코딩/run-length-encoding.js
@@ -1,20 +1,23 @@
 const fs = require("fs");
 
-const string = fs.readFileSync(0).toString().trim();
+const string = fs.readFileSync(0, "utf-8").trim();
 
 let idx = 0;
 const n = string.length;
-let result = "";
+const result = [];
 
 while (idx < n) {
-    let char = string[idx];
+    const char = string[idx];
     let count = 0;
-    while (idx < n && char == string[idx]) {
+
+    while (idx < n && string[idx] === char) {
         idx++;
         count++;
     }
-    result = result + char + count;
+
+    result.push(char, count);
 }
 
-console.log(result.length);
-console.log(result);
+const compressedString = result.join("");
+console.log(compressedString.length);
+console.log(compressedString);
