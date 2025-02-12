1d092bee493ffdd09344c5c02198abbced86540b
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-12 15:05:47 +0900

    Title: 문자열에서 문자 출력, Time: 104ms, Memory: 9MB, Status: Passed - CodeTree

--- 250212/문자열에서 문자 출력/print-chars-from-word.java
+++ 250212/문자열에서 문자 출력/print-chars-from-word.java
@@ -5,11 +5,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         String str = br.readLine();
 
         for (int i = 0; i < str.length(); i++) {
-            bw.append(str.charAt(i)).append("\n");
+            sb.append(str.charAt(i)).append("\n");
         }
+        bw.append(sb.toString());
         bw.close();
     }
 }
