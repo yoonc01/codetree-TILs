73c9d19cdb1040ba4146298a42859d4c1335dd00
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-16 15:07:06 +0900

    Title: a로 채워넣기, Time: 176ms, Memory: 15MB, Status: Passed - CodeTree

--- 250216/a로 채워넣기/filling-with-a.js
+++ 250216/a로 채워넣기/filling-with-a.js
@@ -2,11 +2,13 @@ const fs = require("fs");
 const str = fs.readFileSync(0, "utf8").trim();
 
 /* 
-* 아래와 같이 split을 써도 되지만 spread 문법을 써도 됨
+* 아래와 같이 split을 써도 되지만 spread 문법이나 구조 분해 할당을 써도 됨
 * const strArray = str.split("");
+* const strArray = [...str];
+* const [...strArray] = str;
 */
 
-const [...strArray] = str;
+const strArray = [...str];
 
 strArray[1] = 'a';
 strArray[strArray.length - 2] = 'a';
