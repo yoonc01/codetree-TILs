da09dfd80d5ea2bea7482c0fbf4161fd9b883deb
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-16 13:33:06 +0900

    Title: 문자 교체하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250216/문자 교체하기/changing-char.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str1, str2;
+
+    cin >> str1 >> str2;
+
+    str2[0] = str1[0];
+    str2[1] = str1[1];
+    cout << str2 << endl;
+    return 0;
+}
