2998b00c188aa408c8d3e5f1722f8c52f53e95e0
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-22 10:12:38 +0900

    Title: 최소공배수 구하기, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

--- 250222/최소공배수 구하기/find-the-least-common-multiple.java
+++ 250222/최소공배수 구하기/find-the-least-common-multiple.java
@@ -16,10 +16,10 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n, m;
+        int a, b;
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
         System.out.println(lcm(a, b));
     }
 }
