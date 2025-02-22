b0aa223f6e39694b56c01253fa6964c8c1e8b70b
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-22 12:14:08 +0900

    Title: 절댓값으로 변경, Time: 166ms, Memory: 14MB, Status: Runtime Error - CodeTree

---
+++ 250222/절댓값으로 변경/find-the-absolute-value.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(0, "uft8").trim().split("\n");
+
+function getNextLine(input) {
+    let inputIdx = 0;
+    return function() {
+        return input[inputIdx].split(/\s+/);
+    }
+}
+
+const nextLine = getNextLine(input);
+
+const [n] = nextLine().map(Number);
+const arr = nextLine.map(Number);
+
+for (let i = 0; i < n; i++)
+    arr[i] = Math.abs(arr[i]);
+
+console.log(arr.join(" "));
