277c9f2225548055118608007b1db85c4e8442b9
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-22 12:35:02 +0900

    Title: 숫자 차례로 출력하기, Time: 189ms, Memory: 15MB, Status: Passed - CodeTree

--- 250222/숫자 차례로 출력하기/to-output-numerically.js
+++ 250222/숫자 차례로 출력하기/to-output-numerically.js
@@ -1,22 +1,20 @@
-function recursive_up(s, e) {
-    if (s == e) {
-        process.stdout.write(`${s}\n`);
+function up(n) {
+    if (n == 0)
         return;
-    }
-    process.stdout.write(`${s} `);
-    recursive_up(s + 1, e);
+    up(n - 1);
+    process.stdout.write(n + " ");
 }
 
-function recursive_down(s, e) {
-    if (s == e) {
-        process.stdout.write(`${s}\n`);
+function down(n) {
+    if (n == 0)
         return;
-    }
-    process.stdout.write(`${s} `);
-    recursive_down(s - 1, e);
+    process.stdout.write(n + " ");
+    down(n - 1);
 }
 
 const fs = require("fs");
-const n = Number(fs.readFileSync(0, "utf8").trim());
-recursive_up(1, n);
-recursive_down(n, 1);
+const n = Number(fs.readFileSync(0).toString().trim());
+
+up(n);
+console.log();
+down(n);
