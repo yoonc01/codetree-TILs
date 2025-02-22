0832c88e78cb5fe0bf672e4d93a17bf1857d369e
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-22 10:11:45 +0900

    Title: 최소공배수 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250222/최소공배수 구하기/find-the-least-common-multiple.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    static int lcm(int a, int b) {
+        return (a * b / gcd(a, b));
+    }
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n, m;
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        System.out.println(lcm(a, b));
+    }
+}
