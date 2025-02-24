ba928690c3e27ee88f2c5a0bb2af497b124230de
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-24 17:39:25 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250224/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void printCnt(int n) {
+    if (n == 0)
+        return;
+    cout << n;
+    printCnt(n - 1);
+    cout << n;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    printCnt(n);
+    return 0;
+}
