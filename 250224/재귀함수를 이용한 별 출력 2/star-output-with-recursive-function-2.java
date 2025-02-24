0fcc70b418291e3bec10801fef86dd9a450e2cec
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-24 17:57:48 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

--- 250224/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
+++ 250224/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -20,6 +20,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         n = Integer.parseInt(br.readLine());
-        printStar(n);    
+        printStar(n);
+        System.out.print(sb);
     }
 }
