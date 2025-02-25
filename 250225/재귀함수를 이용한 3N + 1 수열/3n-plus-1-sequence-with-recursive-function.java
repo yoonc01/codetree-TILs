a0e719087deedcb7a9ffd36e42424085db57f998
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-25 17:18:25 +0900

    Title: 재귀함수를 이용한 3N + 1 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250225/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.java
+++ 250225/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.java
@@ -14,7 +14,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine())
+        n = Integer.parseInt(br.readLine());
         System.out.println(count(n));
     }
 }
