ce5b792884afbbd1a5eff159af8a0c88886b2216
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-25 17:24:53 +0900

    Title: 이상한 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250225/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+
+    static int seq(int n) {
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        return seq(n / 3) + seq(n - 1);
+    }
+
+    public static void main(String[] args) {
+        n = Integer.parseInt(br.readLine());
+
+        System.out.println(seq(n));
+    }
+}
