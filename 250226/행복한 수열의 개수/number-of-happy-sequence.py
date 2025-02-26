9c53fbc2670afcdf5a484df926d2d0034a985692
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-26 18:07:18 +0900

    Title: 행복한 수열의 개수, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250226/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,31 @@
+n, m = map(int, input().split())
+G = [list(map(int, input().split())) for _ in range(n)]
+ans = 0
+
+for i in range(n):
+    cnt = 1
+    pivot = G[i][0]
+    for j in range(1, n):
+        if G[i][j] == pivot:
+            cnt = cnt + 1
+        else:
+            cnt = 1
+            pivot = G[i][j]
+        if (cnt == m):
+            ans = ans + 1
+            break
+
+for i in range(n):
+    cnt = 1
+    pivot = G[0][i]
+    for j in range(1, n):
+        if G[j][i] == pivot:
+            cnt = cnt + 1
+        else:
+            cnt = 1
+            pivot = G[j][i]
+        if (cnt == m):
+            ans = ans + 1
+            break
+
+print(ans)
