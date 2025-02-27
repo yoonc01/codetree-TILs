d8ff06850724e752cbd9ad8a7a89585197192edb
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-28 08:50:01 +0900

    Title: 계단 오르기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250228/계단 오르기/climbing-stairs.py
+++ 250228/계단 오르기/climbing-stairs.py
@@ -1,11 +1,12 @@
 n = int(input())
 
 dp = [0 for _ in range(n + 1)]
+dp[0] = 1
 
 for i in range(n + 1):
     if i >= 2:
-        dp[i] = max(dp[i - 2] + 1, dp[i])
+        dp[i] = max(dp[i - 2], dp[i])
     if i >= 3:
-        dp[i] = max(dp[i - 3] + 1, dp[i])
+        dp[i] = max(dp[i - 3] + dp[i - 2], dp[i])
 
 print(dp[n] % 10007)
