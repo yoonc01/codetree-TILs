2cd843befdd7ba0c2b430dae8a961f770a2791b3
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-27 13:20:22 +0900

    Title: 양수 직사각형의 최대 크기, Time: 82ms, Memory: 19MB, Status: Passed - CodeTree

--- 250227/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 250227/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -6,23 +6,18 @@ n, m = map(int, input().split())
 G = [list(map(int, input().split())) for _ in range(n)]
 ans = -1
 
-def check_all_positive(x, y, w, h):
-    for i in range(h):
-        for j in range(w):
-            if (G[x + i][y + j] <= 0):
+def check_all_positive(x1, x2, y1, y2):
+    for i in range(x1, x2 + 1):
+        for j in range(y1, y2 + 1):
+            if (G[i][j] <= 0):
                 return False
     return True
 
-def findMax():
-    global ans
+for i in range(n):
+    for j in range(m):
+        for k in range(i, n):
+            for l in range(j, m):
+                if (check_all_positive(i, k, j, l)):
+                    ans = max(ans, (k - i + 1) * (l - j + 1))
 
-    for w in range(1, m + 1):
-        for h in range(1, n + 1):
-            for i in range(n - h + 1):
-                for j in range(m - w + 1):
-                    if (check_all_positive(i, j, w, h)):
-                        ans = max(ans, w * h)
-
-findMax()
 print(ans)
-
