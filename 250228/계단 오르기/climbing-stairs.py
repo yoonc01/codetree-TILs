8d88dec7f4dfa981541fce4096eb7ad952f89a49
yoonc01 <143938662+yoonc01@users.noreply.github.com>
2025-02-28 08:56:13 +0900

    Title: 계단 오르기, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250228/계단 오르기/climbing-stairs.py
+++ 250228/계단 오르기/climbing-stairs.py
@@ -1,19 +1,12 @@
-MAX_N = 1000
-MOD = 10007
-
-# 변수 선언 및 입력:
 n = int(input())
-dp = [0] * (MAX_N + 1)
 
-# 초기 조건 설정
+dp = [0 for _ in range(1001)]
 dp[0] = 1
 dp[1] = 0
 dp[2] = 1
 dp[3] = 1
 
-# 점화식에 따라 dp값 채우기
-# dp[i] = dp[i - 2] + dp[i - 3]
 for i in range(4, n + 1):
-    dp[i] = (dp[i - 2] + dp[i - 3]) % MOD
+        dp[i] = (dp[i - 3] + dp[i - 2]) % 10007
 
 print(dp[n])
