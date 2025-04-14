4d8021ebc11856a79934d7a0297f3c8d4a12e38b
윤효준 😎 <143938662+yoonc01@users.noreply.github.com>
2025-04-14 17:48:42 +0900

    Title: 정상적인 구구단, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250414/정상적인 구구단/normal-multiple.py
+++ 250414/정상적인 구구단/normal-multiple.py
@@ -1,8 +1,7 @@
 n = int(input())
 
 for i in range(1, n + 1):
+    arr = [0 for _ in range(n)]
     for j in range(1, n + 1):
-        print(f"{i} * {j} = {i * j}", end = "")
-        if (j != n):
-            print(", ", end = "")
-    print()
+        arr[j - 1] = f"{i} * {j} = {i * j}"
+    print(", ".join(arr))
